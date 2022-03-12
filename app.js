76453d264675625bf783871380199d3d65e3fc53
Aftab Ali <67967050+aliaftab612@users.noreply.github.com>
2022-03-12 22:39:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const courses = [
 
 coursesRoute(app, courses);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
